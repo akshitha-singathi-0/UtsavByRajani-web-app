f48173483fda898238b0fc07142d0d712e5912e3
akshitha-singathi-0 <as283@njit.edu>
2024-07-31 15:25:29 -0400

    more styling changes--camel case inline styles!

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -3,28 +3,28 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
+<nav class="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:"#dd0075"}}>
+  <a class="navbar-brand" href="#">Utsav by Rajani</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-      <Link class="nav-link" to="/">Home<span class="sr-only">(current)</span></Link>
+      <Link class="nav-link" to="/">Home</Link>
       </li>
       <li class="nav-item">
-      <Link class="nav-link" to="/about">About<span class="sr-only">(current)</span></Link>
+      <Link class="nav-link" to="/about">About</Link>
       </li>
       <li class="nav-item">
-      <Link class="nav-link" to="/gallery">Gallery<span class="sr-only">(current)</span></Link>
+      <Link class="nav-link" to="/gallery">Gallery</Link>
       </li>
       <li class="nav-item">
-      <Link class="nav-link" to="/contact">Contact<span class="sr-only">(current)</span></Link>
+      <Link class="nav-link" to="/contact">Contact</Link>
       </li>
     </ul>
     <span class="navbar-text">
-      Navbar text with an inline element
+      Header
     </span>
   </div>
 </nav>
