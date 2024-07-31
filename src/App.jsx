31441050a6f7ea76706b3ed1f1fb7e133a9258d1
akshitha-singathi-0 <as283@njit.edu>
2024-07-31 13:45:27 -0400

    routing functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Navbar from './assets/Components/Navbar';
+import Home from './assets/Components/Home';
 import About from './assets/Components/About';
 import Contact from './assets/Components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <About></About>
-      <Contact></Contact>
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
