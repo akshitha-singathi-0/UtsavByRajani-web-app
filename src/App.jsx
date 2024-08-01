3eeab3fcf9861068e42f800eb7815d424c5f9ca1
akshitha-singathi-0 <as283@njit.edu>
2024-08-01 10:03:06 -0400

    justified the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
-import Navbar from './assets/components/Navbar';
-import Home from './assets/components/Home';
-import About from './assets/components/About';
-import Gallery from './assets/components/Gallery';
-import Contact from './assets/components/Contact';
-import Footer from './assets/components/Footer';
+import Navbar from './assets/Components/Navbar';
+import Home from './assets/Components/Home';
+import About from './assets/Components/About';
+import Gallery from './assets/Components/Gallery';
+import Contact from './assets/Components/Contact';
+import Footer from './assets/Components/Footer';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-      <div>
+      <div class="App">
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/about" element={<About/>}></Route>
