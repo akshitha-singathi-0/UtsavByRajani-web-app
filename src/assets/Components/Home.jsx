2a3bc3176a45b6bb7f27a0d8e3e143d829b62573
akshitha-singathi-0 <as283@njit.edu>
2024-07-31 22:41:09 -0400

    added barebones to home

--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -7,7 +7,19 @@ const Home = () => {
                 <h1>Compelling Headline</h1>
                 <button>View Gallery</button>
             </div>
-
+            <div>
+                <h3>Engaging Subheading</h3>
+            </div>
+            <div>
+                <p>Here is a mini paragraph that tells our story.</p>
+                <button>Read More</button>
+            </div>
+            <div>
+                <div>Card1</div>
+                <div>Card2</div>
+                <div>Card3</div>
+            </div>
+            <div>Social Media Link</div>
         </div>
     );
 }
