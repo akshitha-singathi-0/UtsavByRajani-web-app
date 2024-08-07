1648fac09c77769137dbb8eb402e934a64080aa8
akshitha-singathi-0 <as283@njit.edu>
2024-08-06 21:22:19 -0400

    image filtering in database but not based on dropdown

--- src/assets/Components/Gallery.jsx
+++ src/assets/Components/Gallery.jsx
@@ -1,32 +1,8 @@
 import React from 'react';
 import ResponsiveGallery from 'react-responsive-gallery';
 import image_data from '../data/image_data.json';
-/*
-const media=[
 
-{
-src: 'https://cdn.pixabay.com/photo/2014/07/08/12/36/bird-386725_960_720.jpg'
-},
-{
-src: 'https://cdn.pixabay.com/photo/2015/10/12/15/46/fallow-deer-984573_960_720.jpg'
-},
-{
-src: 'https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg'
-},
-{
-src: 'https://cdn.pixabay.com/photo/2013/09/22/15/29/prairie-dog-184974_960_720.jpg'
-},
-{
-src: 'https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_960_720.jpg'
-},
-{
-src: 'https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg'
-},
-{
-src: 'https://cdn.pixabay.com/photo/2019/03/09/17/30/horse-4044547_960_720.jpg'
-}
-];
-*/
+const data_by_filter = image_data.filter(item => item.eventType === 'wedding');
 
 const Gallery = () => {
     return (
@@ -47,7 +23,7 @@ const Gallery = () => {
                 </button>
             </div>
             <div>
-                <ResponsiveGallery media={image_data}></ResponsiveGallery>
+                <ResponsiveGallery media={data_by_filter}></ResponsiveGallery>
             </div>
         </div>
     );
