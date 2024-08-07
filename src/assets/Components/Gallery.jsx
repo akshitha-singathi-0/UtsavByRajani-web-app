bfc375ee2484017c55bf9cc68e2b19f05602674c
akshitha-singathi-0 <as283@njit.edu>
2024-08-07 11:13:50 -0400

    functional gallery with filtering using state

--- src/assets/Components/Gallery.jsx
+++ src/assets/Components/Gallery.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ResponsiveGallery from 'react-responsive-gallery';
 import image_data from '../data/image_data.json';
 
-const data_by_filter = image_data.filter(item => item.eventType === 'wedding');
-
 const Gallery = () => {
+    
+    const [selectedFilter, setSelectedFilter] = useState('all');
+    const [filteredData, setFilteredData] = useState(image_data);
+
+    const handleSelectedFilter = (filter) => {
+        setSelectedFilter(filter);
+        const data_after_filtering = filter === 'all' ? image_data : image_data.filter(item => item.eventType === filter);
+        setFilteredData(data_after_filtering);
+    }
+
     return (
         <div>
             <h1 class="pageHeader">Gallery</h1>
@@ -13,17 +21,18 @@ const Gallery = () => {
                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Filter by
                     </a>
-                    <ul className="dropdown-menu">
-                    <li><a className="dropdown-item" href="#">Wedding Events</a></li>
-                    <li><a className="dropdown-item" href="#">Arangetram/Rangapravesam</a></li>
-                    <li><a className="dropdown-item" href="#">Birthday parties</a></li>
+                    <ul className="dropdown-menu">                    
+                    <li><a className="dropdown-item" href="#" onClick={() => handleSelectedFilter('all')}>All</a></li>
                     <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="#">Other</a></li>
+                    <li><a className="dropdown-item" href="#" onClick={() => handleSelectedFilter('wedding')}>Wedding Events</a></li>
+                    <li><a className="dropdown-item" href="#" onClick={() => handleSelectedFilter('arangetram')}>Arangetram/Rangapravesam</a></li>
+                    <li><a className="dropdown-item" href="#" onClick={() => handleSelectedFilter('party')}>Birthday parties</a></li>
+                    <li><a className="dropdown-item" href="#" onClick={() => handleSelectedFilter('other')}>Other</a></li>
                     </ul>
                 </button>
             </div>
             <div>
-                <ResponsiveGallery media={data_by_filter}></ResponsiveGallery>
+                <ResponsiveGallery media={filteredData}></ResponsiveGallery>
             </div>
         </div>
     );
